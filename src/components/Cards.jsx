398607ac5da87bad5f306ae7b5869c7eca32a105
Dimas Julio Cessar <dimasjc08@gmail.com>
2023-03-09 16:30:46 +0700

    fix cards.jsx

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -6,9 +6,9 @@ import Pajak from '../assets/Icon/tax.png'
 const Cards = () => {
   return (
     <div className='w-full py-[1rem] px-4'>
-        <div className='row-2 max-w-[1240px] text-right mx-[5px] md:mx-[90px] sm:mx-[180px] mb-[20px] sm:mb-[50px]'>
-            <h1 className='text-[#FFFFFF] font-bold text-[30px] sm:text-[64px]'>Temukan informasi terkini dan terpercaya di <a className='text-[#3CC602]'>website</a> kami!</h1>
-            <h3 className='text-[#9999A1] text-[16px] sm:text-[24px]'>Segera temukan jawaban atas pertanyaan Anda di sini</h3>
+        <div className='row-2 max-w-full text-right mx-[5px] md:mx-[90px] sm:mx-[180px] mb-[20px] sm:mb-[50px] px-[0px] md:px-[350px] sm:px-[350px]'>
+            <h1 className='text-[#FFFFFF] font-bold text-[30px] md:text-[64px] '>Temukan informasi terkini dan terpercaya di <a className='text-[#3CC602]'>website</a> kami!</h1>
+            <h3 className='text-[#9999A1] text-[16px] sm:text-[24px] '>Segera temukan jawaban atas pertanyaan Anda di sini</h3>
         </div>
       <div className='max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8'>
           <div className='max-w-[400px] w-full mx-auto bg-[#18181B] sm:items-center sm:content-start sm:justify-item-center stroke-3 stroke-[#272728] shadow-xl flex flex-col p-4 my-4 rounded-tl-[40px] rounded-br-[40px] hover:scale-105 duration-300'>
