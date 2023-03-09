7494f288592272918fa7f65b3db5e35944e937a5
Dimas Julio Cessar <dimasjc08@gmail.com>
2023-03-09 15:42:36 +0700

    memperbaiki card.jsx

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -11,19 +11,19 @@ const Cards = () => {
             <h3 className='text-[#9999A1] text-[16px] sm:text-[24px]'>Segera temukan jawaban atas pertanyaan Anda di sini</h3>
         </div>
       <div className='max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8'>
-          <div className='w-full bg-[#18181B] sm:items-center sm:content-start sm:justify-item-center stroke-3 stroke-[#272728] shadow-xl flex flex-col p-4 my-4 rounded-tl-[40px] rounded-br-[40px] hover:scale-105 duration-300'>
+          <div className='max-w-[400px] w-full mx-auto bg-[#18181B] sm:items-center sm:content-start sm:justify-item-center stroke-3 stroke-[#272728] shadow-xl flex flex-col p-4 my-4 rounded-tl-[40px] rounded-br-[40px] hover:scale-105 duration-300'>
               <img className='w-[150px] mx-auto mt-[2rem]' src={Asuransi} alt="/" />
               <h2 className='text-[40px] font-bold text-center py-8 text-white cursor-default'>Asuransi</h2>
               <p className='w-[280px] md:w-auto h-[100px] md:h-auto md:ml-2 text-left text-lg text-[#9999A1] cursor-default'>Informasi seputar asuransi ketenagakerjaan dan kesehatan, banyak hal yang dapat diketahui ...</p>
               <button className='bg-[#00df9a] w-auto rounded-md text-[18px] font-semibold my-6 mx-auto px-6 py-3'>Baca Selengkapnya</button>
           </div>
-          <div className='w-full bg-[#18181B] sm:items-center sm:content-start sm:justify-item-center stroke-3 stroke-[#272728] shadow-xl flex flex-col p-4 md:my-0 my-8 rounded-tl-[40px] rounded-br-[40px] hover:scale-105 duration-300'>
+          <div className='max-w-[400px] w-full mx-auto bg-[#18181B] sm:items-center sm:content-start sm:justify-item-center stroke-3 stroke-[#272728] shadow-xl flex flex-col p-4 md:my-0 my-8 rounded-tl-[40px] rounded-br-[40px] hover:scale-105 duration-300'>
               <img className='w-[150px] mx-auto mt-[2rem]' src={Legal} alt="/" />
               <h2 className='text-[40px] font-bold text-center py-8 text-white cursor-default'>Legal Dokumen</h2>
               <p className='w-[280px] md:w-auto h-[100px] md:h-auto md:ml-2 text-left text-lg text-[#9999A1] mb-[25px] cursor-default'>Informasi seputar asuransi ketenagakerjaan dan kesehatan, banyak hal yang dapat diketahui ...</p>
               <button className='bg-[#00df9a] w-auto rounded-md text-[18px] font-semibold my-6 mx-auto px-6 py-3'>Baca Selengkapnya</button>
           </div>
-          <div className='w-full bg-[#18181B] sm:items-center sm:content-start sm:justify-item-center stroke-[#272728] shadow-xl flex flex-col p-4 my-4 rounded-tl-[40px] rounded-br-[40px] hover:scale-105 duration-300'>
+          <div className='max-w-[400px] w-full mx-auto bg-[#18181B] sm:items-center sm:content-start sm:justify-item-center stroke-[#272728] shadow-xl flex flex-col p-4 my-4 rounded-tl-[40px] rounded-br-[40px] hover:scale-105 duration-300'>
               <img className='w-[150px] mx-auto mt-[2rem]' src={Pajak} alt="/" />
               <h2 className='text-[40px] font-bold text-center py-8 text-white cursor-default'>Perpajakan</h2>
               <p className='w-[280px] md:w-auto h-[100px] md:h-auto md:ml-2 text-left text-lg text-[#9999A1] cursor-default'>Informasi seputar asuransi ketenagakerjaan dan kesehatan, banyak hal yang dapat diketahui ...</p>
